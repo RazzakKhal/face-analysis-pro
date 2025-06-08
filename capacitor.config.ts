877705d431d50ff838129d08762bbfcab129ae26
razzak <khalfallah.razzak@gmail.com>
2025-06-08 23:34:54 +0200

    changement de nom

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Face Analysis Pro',
+  appId: 'com.goldenratio.analyzer', // identifiant unique du package
+  appName: 'Golden Ratio Face Analyzer', // nom affiché sur le téléphone
   webDir: 'www'
 };
 
