e8bbc1ff873b31087a11d353fea740c76f50e008
razzak <khalfallah.razzak@gmail.com>
2025-06-11 19:02:36 +0200

    android update

--- src/app/tabs/scan/scan.page.ts
+++ src/app/tabs/scan/scan.page.ts
@@ -5,6 +5,7 @@ import { AnalyzeApiService } from 'src/app/handlers/analyze-api.service';
 import { StorageHandlerService } from 'src/app/handlers/storage-handler.service';
 import { forkJoin, interval, Observable, Subscription, takeWhile, tap } from 'rxjs';
 import { PhotoHandlerService } from 'src/app/handlers/photo-handler.service';
+import { AdMob, RewardAdPluginEvents } from '@capacitor-community/admob';
 
 @Component({
   selector: 'app-scan',
@@ -17,7 +18,7 @@ export class ScanPage {
   currentPhoto: any;
   progress = 0;
   loadingDone = false;
-  private progressSubscription? : Subscription
+  private progressSubscription?: Subscription
 
   constructor(
     private analyzeApiService: AnalyzeApiService,
@@ -39,18 +40,18 @@ export class ScanPage {
   }
 
   simulateProgressUntil100() {
-  this.progress = 0;
+    this.progress = 0;
 
-  return interval(200).pipe(
-    takeWhile(() => this.progress < 99),
-    tap(() => {
-      const increment = this.progress < 90
-        ? Math.floor(Math.random() * 8) + 1
-        : 1;
+    return interval(200).pipe(
+      takeWhile(() => this.progress < 99),
+      tap(() => {
+        const increment = this.progress < 90
+          ? Math.floor(Math.random() * 8) + 1
+          : 1;
 
-      this.progress += increment;
-    })
-  );
+        this.progress += increment;
+      })
+    );
   }
 
   async makeAnalyze() {
@@ -66,13 +67,31 @@ export class ScanPage {
     const api$ = this.getReportFromApi(photo.webPath!);
 
     forkJoin([progress$, api$]).subscribe({
-      next: () => {
+      next: async () => {
+        await AdMob.initialize();
+
+        // Charger l’interstitielle
+        await AdMob.prepareRewardInterstitialAd({
+          adId: 'ca-app-pub-4427716091717352/6864935401', // ton ad unit ID
+          isTesting: true // ⛔ change à false pour prod
+        });
+
+        // 🔁 Écouter si la récompense a été validée
+        const listener = AdMob.addListener(RewardAdPluginEvents.Rewarded, async () => {
+          console.log('🎁 L’utilisateur a bien regardé la pub');
           this.router.navigateByUrl('/tabs/report');
+
+          // Nettoyer l'écouteur après usage
+          (await listener).remove();
+        });
+        // Montrer la pub après le scan
+        await AdMob.showRewardInterstitialAd();
+        this.router.navigateByUrl('/tabs/report');
       },
       error: async (err) => {
-          this.progress = 0;
-          await this.notifyError(err);
-          this.router.navigateByUrl('/tabs/takepicture');
+        this.progress = 0;
+        await this.notifyError(err);
+        this.router.navigateByUrl('/tabs/takepicture');
       }
     });
 
@@ -95,22 +114,22 @@ export class ScanPage {
    * @param webPath 
    * @returns 
    */
-   getReportFromApi(webPath: string) {
+  getReportFromApi(webPath: string) {
     return new Observable(observer => {
-    this.photoHandlerService.getBlobFromPhoto(webPath).then(blob => {
-      this.analyzeApiService.makeAnalyze(blob).subscribe({
-        next: async (response) => {
-          const zip = await this.analyzeApiService.getZipContent(response);
-          const reportId = `report_${Date.now()}`;
-          await this.storageHandlerService.saveReportMetadata(reportId, zip);
-          await this.storageHandlerService.saveReportImages(reportId, zip);
-          await this.storageHandlerService.saveCurrentReportId(reportId);
-          observer.next(undefined);
-          observer.complete();
-        },
-        error: (error) => observer.error(error)
+      this.photoHandlerService.getBlobFromPhoto(webPath).then(blob => {
+        this.analyzeApiService.makeAnalyze(blob).subscribe({
+          next: async (response) => {
+            const zip = await this.analyzeApiService.getZipContent(response);
+            const reportId = `report_${Date.now()}`;
+            await this.storageHandlerService.saveReportMetadata(reportId, zip);
+            await this.storageHandlerService.saveReportImages(reportId, zip);
+            await this.storageHandlerService.saveCurrentReportId(reportId);
+            observer.next(undefined);
+            observer.complete();
+          },
+          error: (error) => observer.error(error)
+        });
       });
     });
-  });
   }
 }
