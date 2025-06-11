3888abe2d344ec02dd5661bb9c563cd591341677
RazzakKhal <khalfallah.razzak@gmail.com>
2025-06-12 01:33:31 +0200

    rename of bundle id and add package json commands

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.goldenratio.analyzer', // identifiant unique du package
+  appId: 'com.goldenratio.analysis', // identifiant unique du package
   appName: 'Golden Ratio Face Analyzer', // nom affiché sur le téléphone
   webDir: 'www'
 };
