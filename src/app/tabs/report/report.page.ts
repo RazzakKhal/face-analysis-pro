5f2c1e24463c8aa71cbf085bfe778179d70215e5
razzak <khalfallah.razzak@gmail.com>
2025-06-11 19:13:29 +0200

    updating of vertical face

--- src/app/tabs/report/report.page.ts
+++ src/app/tabs/report/report.page.ts
@@ -56,11 +56,11 @@ export class ReportPage {
   }
 
   getVerticalRatioMessage(tier1: number, tier2: number, tier3: number, tier4: number, tier5: number): string {
-    const ratio1 = (tier1 / tier3).toFixed(2);
+    // const ratio1 = (tier1 / tier3).toFixed(2);
     const ratio2 = (tier2 / tier3).toFixed(2);
     const ratio3 = (tier4 / tier3).toFixed(2);
-    const ratio4 = (tier5 / tier3).toFixed(2);
-    return `A balanced vertical facial ratio — where the eye width, inter-eye distance, and outer spacing align — is often seen in attractive faces. Your current proportions are ${ratio1} : ${ratio2} : 1 : ${ratio3} : ${ratio4}.`;
+    // const ratio4 = (tier5 / tier3).toFixed(2);
+    return `A balanced vertical facial ratio — where the eye width and inter-eye distance— is often seen in attractive faces. Your current proportions are ${ratio2} : 1 : ${ratio3}.`;
   }
 
   async presentCongratulationPopup() {
