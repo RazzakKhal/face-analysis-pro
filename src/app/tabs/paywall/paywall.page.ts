e6bcd69dae1f62fb32cc84ad3a980c7e47614296
RazzakKhal <khalfallah.razzak@gmail.com>
2025-06-13 01:32:45 +0200

    add of paywall page and toggle

--- src/app/tabs/paywall/paywall.page.ts
+++ src/app/tabs/paywall/paywall.page.ts
@@ -8,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaywallPage implements OnInit {
 
+  yearlyPlan = false;
+  weeklyPlan = true;
+  freeTrial = true;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  selectedPlan(plan: string){
+    if(plan === "weekly"){
+      this.weeklyPlan = true;
+      this.yearlyPlan = false;
+    }else{
+      this.weeklyPlan = false;
+      this.yearlyPlan = true;
+    }
+  }
+
+  freeTrialToggle(){
+    this.freeTrial = !this.freeTrial
+    console.log('free trial : ', this.freeTrial)
+  }
 }
